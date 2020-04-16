c5ae54a8ef27c5efc255714b4346d7931b1a31e1
Kong, Zelun <zelun.kong@outlook.com>
2020-04-16 13:18:19 -0500

    scripts

--- scripts/base_build.sh
+++ scripts/base_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # 
 # base_build.sh
 #
@@ -7,21 +7,70 @@ set -exu -o pipefail
 
 LFS=/mnt/lfs
 
-builddir=$(pwd)
-log="$builddir"/base.log
+scriptdir=$(pwd)
+logdir="$scriptdir"/../log
 pkgs=(
     base
+    iana-etc
     linux-api-headers
     glibc
+    tzdata
+    gcc
+    bash
+    coreutils
+    util-linux
+    file
+    findutils
+    procps-ng
+    psmisc
+    shadow
+    tar
+    bzip2
+    gzip
+    xz
+    grep
+    sed
+    gawk
+    systemd
+    pciutils
+    iputils
+    iproute2
+    autoconf
+    automake
+    binutils
+    bison
+    fakeroot
+    flex
+    gettext
+    libtool
+    m4
+    make
+    meson
+    ninja
+    patch
+    pkgconf
+    sudo
+    which
+    perl
+    python
+    git
+    pacman
 )
+
+if [[ -e $logdir ]]; then
+    rm -rf $logdir
+fi
+
+mkdir -p $logdir
 
 for p in ${pkgs[@]}
 do
-    echo ${p}
-    cd "${builddir}"/${p}
-    gpg --recv-keys $(grep -E -o "[0-9A-F]{40}" PKGBUILD)
-    makepkg --config ${builddir}/makepkg-core.conf -Cf &> $log
-    sudo repo-add $LFS/pkgs/core/core.db.tar.gz $LFS/pkgs/core/${p}-*.pkg.tar.gz &> $log
+    echo $p
+    log="$logdir"/$p.log
+    cd "$scriptdir"/../base/$p
+    # gpg --recv-keys $(grep -E -o "[0-9A-F]{40}" PKGBUILD)
+    makepkg --config "$scriptdir"/../config/makepkg-base.conf -cCf &>> $log
+    sudo repo-add $LFS/pkgs/base/base.db.tar.gz $LFS/pkgs/base/$p-*.pkg.tar.gz &>> $log
 done
 
-sudo pacstrap -C ../config/pacman-lfs.conf /mnt/lfs base || exit
+sudo pacstrap -C ../config/pacman-lfs.conf /mnt/lfs base
