f50957abf808455b93e58a7b5b8aebd114518e7a
Kong, Zelun <zelun.kong@outlook.com>
2020-04-26 17:38:28 -0500

    config and scripts

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -10,163 +10,6 @@ LFS=/mnt/lfs
 scriptdir=$(pwd)
 logdir="$scriptdir"/../log
 
-base_pkgs=(
-    autoconf
-    automake
-    base
-    bash
-    binutils
-    bison
-    bzip2
-    cmake
-    coreutils
-    dbus
-    e2fsprogs
-    fakeroot
-    file
-    findutils
-    flex
-    gawk
-    gcc
-    gettext
-    git
-    glibc
-    grep
-    gzip
-    iana-etc
-    iproute2
-    iputils
-    libtool
-    linux-api-headers
-    m4
-    make
-    meson
-    ncurses
-    ninja
-    pacman
-    patch
-    pciutils
-    perl
-    pkgconf
-    procps-ng
-    psmisc
-    python
-    readline
-    sed
-    shadow
-    sudo
-    systemd
-    tar
-    tzdata
-    util-linux
-    which
-    xz
-    zlib minizip
-)
-
-core_pkgs=(
-    acl
-    argon2
-    attr
-    audit
-    bc
-    boost
-    ca-certificates
-    cracklib
-    cryptsetup
-    curl
-    cython
-    db
-    diffutils
-    ed
-    elfutils
-    expat
-    gc
-    gdb
-    gdbm
-    glib2
-    gmp
-    gnupg
-    gnutls
-    gnu-efi
-    gperf
-    gpgme
-    guile
-    hidapi
-    icu
-    iptables
-    itstool
-    jsoncpp
-    json-c
-    kbd
-    keyutils
-    kmod
-    krb5
-    lapack
-    less
-    libaio
-    libarchive
-    libassuan
-    libcap
-    libcap-ng
-    libcbor
-    libedit
-    libffi
-    libfido2
-    libgcrypt
-    libgpg-error
-    libidn2
-    libksba
-    libmicrohttpd
-    libmnl
-    libnetfilter_conntrack
-    libnfnetlink
-    libnftnl
-    libnghttp2
-    libnl
-    libnsl
-    libpcap
-    libpng
-    libpsl
-    libsasl
-    libseccomp
-    libssh2
-    libtasn1
-    libtirpc
-    libunistring
-    libusb
-    libusb-compat
-    libuv
-    libxml2
-    lvm2
-    lz4
-    mpc
-    mpfr
-    nettle
-    npth
-    nss
-    openldap
-    openssh
-    openssl
-    p11-kit
-    pam
-    pcre
-    pcre2
-    pcsclite
-    popt
-    publicsuffix-list
-    python-numpy
-    qrencode
-    rhash
-    shared-mime-info
-    sqlite
-    swig
-    tcl
-    thin-provisioning-tools
-    valgrind
-    zstd
-)
-
 build() {
     log="$logdir"/$2.log
     if [[ -f $log ]]; then
@@ -179,10 +22,6 @@ build() {
 }
 
 case $1 in
-    base)
-        pkgs=${base_pkgs[@]}
-        repo='base'
-        ;;
     core)
         pkgs=${core_pkgs[@]}
         repo='core'
@@ -197,8 +36,6 @@ if [[ ! -d $logdir ]]; then
     mkdir -p $logdir
 fi
 
-for p in ${pkgs[@]}
-do
+for p in $(ls --format=single-column ../$repo); do
     build $repo $p
-    # sudo repo-add $LFS/pkgs/$repo.db.tar.gz $LFS/pkgs/$p-*.pkg.tar.gz &>> $log
 done
--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -7,92 +7,62 @@ set -exu -o pipefail
 
 LFS=/mnt/lfs
 
-base_pkgs=(
-    autoconf
-    automake
-    base
-    bash
-    binutils
-    bison
-    bzip2
-    cmake
-    coreutils
-    dbus
-    e2fsprogs
-    fakeroot
-    file
-    findutils
-    flex
-    gawk
-    gcc
-    gettext
-    git
-    glibc
-    grep
-    gzip
-    iana-etc
-    iproute2
-    iputils
-    libtool
-    linux
-    linux-api-headers
-    m4
-    make
-    meson
-    ncurses
-    ninja
-    pacman
-    patch
-    pciutils
-    perl
-    pkgconf
-    procps-ng
-    psmisc
-    python
-    readline
-    sed
-    shadow
-    sudo
-    systemd
-    tar
-    tzdata
-    util-linux
-    which
-    xz
-    zlib
-)
-
-core_pkgs=(
+pkgs=(
     acl
     argon2
     attr
     audit
+    autoconf
+    automake
+    base
+    bash
     bc
+    binutils
+    bison
     boost
+    bzip2
     ca-certificates
+    cmake
+    coreutils
     cracklib
     cryptsetup
     curl
     cython
     db
+    dbus
     diffutils
+    e2fsprogs
     ed
     elfutils
     expat
+    fakeroot
+    file
+    findutils
+    flex
+    gawk
     gc
+    gcc
     gdb
     gdbm
+    gettext
+    git
     glib2
+    glibc
     gmp
     gnupg
     gnutls
     gnu-efi
     gperf
     gpgme
+    grep
     guile
+    gzip
     hidapi
+    iana-etc
     icu
+    iproute2
     iptables
+    iputils
     itstool
     jsoncpp
     json-c
@@ -131,63 +101,76 @@ core_pkgs=(
     libssh2
     libtasn1
     libtirpc
+    libtool
     libunistring
     libusb
     libusb-compat
     libuv
     libxml2
+    linux
+    linux-api-headers
     lvm2 device-mapper
     lz4
+    m4
+    make
+    meson
     mpc
     mpfr
+    nano
+    ncurses
     nettle
+    ninja
     npth
     nss
-    libldap #openldap
+    libldap # openldap
     openssh
     openssl
     p11-kit
+    pacman
     pam
+    patch
+    pciutils
     pcre
     pcre2
     pcsclite
+    perl
+    pkgconf
     popt
+    procps-ng
+    psmisc
     publicsuffix-list
+    python
     python-numpy
     qrencode
+    readline
     rhash
+    sed
+    shadow
     shared-mime-info
     sqlite
+    sudo
     swig
+    systemd
+    tar
     tcl
     thin-provisioning-tools
+    tzdata
+    util-linux
     valgrind
+    which
+    xz
+    zlib minizip
     zstd
 )
 
 update() {
-    sudo repo-add -R $LFS/pkgs/$1.db.tar.gz $LFS/pkgs/$2-*.pkg.tar.gz
+    sudo repo-add -R $LFS/pkgs/core.db.tar.gz $LFS/pkgs/$1-*.pkg.tar.gz
 }
 
-case $1 in
-    base)
-        pkgs=${base_pkgs[@]}
-        repo='base'
-        ;;
-    core)
-        pkgs=${core_pkgs[@]}
-        repo='core'
-        ;;
-    *)
-        # unknow repo
-        exit 1
-        ;;
-esac
-
-for r in $(find $LFS/pkgs -name "$repo.db*"); do
+for r in $(find $LFS/pkgs -name "core.*"); do
     rm -f $r
 done
 
 for p in ${pkgs[@]}; do
-    update $repo $p
+    update $p
 done
