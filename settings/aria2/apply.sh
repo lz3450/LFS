d663542aeff698358a5954b9d6bf8bd9dd297e8e
Kong, Zelun <zelun.kong@outlook.com>
2020-11-09 06:36:52 +0000

    settings: aria2 add webui service

--- settings/aria2/apply.sh
+++ settings/aria2/apply.sh
@@ -6,8 +6,17 @@ mkdir -p ~/.config/systemd/user/
 
 cp aria2.conf ~/.config/aria2/aria2.conf
 cp aria2-rpc.conf ~/.config/aria2/aria2-rpc.conf
-cp aria2cd.service ~/.config/systemd/user/aria2cd.service
+cp aria2cd.service ~/.config/systemd/user/
+cp aria2-webui.service ~/.config/systemd/user/
 touch ~/aria2/aria2.log
 touch ~/aria2/aria2.session
 touch ~/aria2/aria2-rpc.log
 touch ~/aria2/aria2-rpc.session
+
+if [ ! -d ~/aria2/webui-aria2 ]; then
+    cd ~/aria2
+    git clone https://github.com/ziahamza/webui-aria2.git
+else
+    cd ~/aria2/webui-aria2
+    git pull
+fi
