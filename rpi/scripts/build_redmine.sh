0a7b51480a7c541254e11e580ce87208525034d5
Kong, Zelun <zelun.kong@outlook.com>
2020-11-09 10:01:35 +0000

    rpi: redmine to outlook

--- rpi/scripts/build_redmine.sh
+++ rpi/scripts/build_redmine.sh
@@ -7,8 +7,6 @@ REDMINE_HOME=~/redmine
 
 REDMINEVER=4.1.1
 
-sudo mount -o remount,size=16G tmpfs $BUILDDIR
-
 sudo apt install -y \
     build-essential patch \
     zlib1g-dev liblzma-dev libpq5 libpq-dev \
@@ -29,16 +27,74 @@ mkdir -p $REDMINE_HOME
 cd $REDMINE_HOME
 tar -xf $BUILDDIR/redmine-$REDMINEVER.tar.gz
 
+sudo -u postgres createdb -O redmine redmine
+
 cd redmine-$REDMINEVER
 cd config
 cp database.yml.example database.yml
-nano database.yml
+cp configuration.yml.example configuration.yml
+tee database.yml << EOF
+production:
+  adapter: postgresql
+  database: redmine
+  host: localhost
+  username: redmine
+  password: "kzl@redmine"
+EOF
+
+mkdir -p $REDMINE_HOME/attachments
+tee configuration.yml << EOF
+default:
+  email_delivery:
+    delivery_method: :smtp
+    smtp_settings:
+      enable_starttls_auto: true
+      address: smtp.office365.com
+      port: 587
+      domain: kzl.net
+      authentication: :login
+      user_name: zelun.kong@outlook.com
+      password: K31405302zl
+
+  attachments_storage_path: /home/kzl/redmine/attachments
+
+  autologin_cookie_name:
+  autologin_cookie_path:
+  autologin_cookie_secure:
+
+  scm_subversion_command:
+  scm_mercurial_command:
+  scm_git_command:
+  scm_cvs_command:
+  scm_bazaar_command:
+
+  scm_subversion_path_regexp:
+  scm_mercurial_path_regexp:
+  scm_git_path_regexp:
+  scm_cvs_path_regexp:
+  scm_bazaar_path_regexp:
+  scm_filesystem_path_regexp:
+
+  scm_stderr_log_file:
+
+  database_cipher_key:
+
+  minimagick_font_path:
+
+production:
+
+development:
+
+EOF
 
 cd $REDMINE_HOME/redmine-$REDMINEVER
 # sudo gem install bundler
 bundle install --without development test
 bundle exec rake generate_secret_token
 RAILS_ENV=production bundle exec rake db:migrate
+# reset database
+# RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset
+RAILS_ENV=production bundle exec rake redmine:load_default_data
 # test 
 #bundle exec rails server webrick -e production
 
@@ -57,3 +113,7 @@ Restart=always
 [Install]
 WantedBy=default.target
 EOF
+
+systemctl --user daemon-reload
+systemctl --user start redmine.service
+systemctl --user enable redmine.service
