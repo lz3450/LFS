1eb577f7e8140d7ac930e89dc76d1b6f3d3758c6
Kong, Zelun <zelun.kong@outlook.com>
2020-11-11 14:11:35 -0600

    settings: aria2

--- settings/aria2/apply.sh
+++ settings/aria2/apply.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -p ~/aria2/downloads
+ARIA2_HOME="$HOME/.aria2"
+
+mkdir -p $ARIA2_HOME/downloads
 mkdir -p ~/.config/aria2/
 mkdir -p ~/.config/systemd/user/
 
@@ -8,15 +10,15 @@ cp aria2.conf ~/.config/aria2/aria2.conf
 cp aria2-rpc.conf ~/.config/aria2/aria2-rpc.conf
 cp aria2cd.service ~/.config/systemd/user/
 cp aria2-webui.service ~/.config/systemd/user/
-touch ~/aria2/aria2.log
-touch ~/aria2/aria2.session
-touch ~/aria2/aria2-rpc.log
-touch ~/aria2/aria2-rpc.session
+touch $ARIA2_HOME/aria2.log
+touch $ARIA2_HOME/aria2.session
+touch $ARIA2_HOME/aria2-rpc.log
+touch $ARIA2_HOME/aria2-rpc.session
 
-if [ ! -d ~/aria2/webui-aria2 ]; then
-    cd ~/aria2
+if [ ! -d $ARIA2_HOME/webui-aria2 ]; then
+    cd $ARIA2_HOME
     git clone https://github.com/ziahamza/webui-aria2.git
 else
-    cd ~/aria2/webui-aria2
+    cd $ARIA2_HOME/webui-aria2
     git pull
 fi
