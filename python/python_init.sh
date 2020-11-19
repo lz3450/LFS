f9dac1de6e1dd0f9de27cf192527e03dc124fb62
Kong, Zelun <zelun.kong@outlook.com>
2020-11-19 00:42:53 -0600

    python

--- python/python_init.sh
+++ python/python_init.sh
@@ -5,8 +5,8 @@ set -e
 # ROOTDIR=/dev/shm
 # PYTHONVER=3.9.0
 
-PYTHONBIN=/usr/local/bin/python3.9
-PIPBIN=/usr/local/bin/pip3.9
+# PYTHONBIN=/usr/local/bin/python3.9
+PYTHONBIN=/usr/bin/python3.6
 
 # sudo apt update
 # sudo apt install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev valgrind liblzma-dev libgdbm-compat-dev libjpeg-dev libffi-dev libmpdec-dev
@@ -37,11 +37,11 @@ PIPBIN=/usr/local/bin/pip3.9
 # sudo ln -sf pip${PYTHONVER:0:3} /usr/local/bin/pip
 # sudo ldconfig
 
-sudo -H $PIPBIN install -U --no-binary :all: pip setuptools wheel
-$PIPBIN install --user -U --no-binary :all: numpy &
-$PIPBIN install --user -U --no-binary :all: pandas &
-$PIPBIN install --user -U --no-binary :all: matplotlib &
-$PIPBIN install --user -U --no-binary :all: jupyter &
-$PIPBIN install --user -U --no-binary :all: ipython &
+sudo -H $PYTHONBIN -m pip install -U --no-binary :all: pip setuptools wheel
+$PYTHONBIN -m pip install --user -U --no-binary :all: numpy &
+$PYTHONBIN -m pip install --user -U --no-binary :all: pandas &
+$PYTHONBIN -m pip install --user -U --no-binary :all: matplotlib &
+$PYTHONBIN -m pip install --user -U --no-binary :all: jupyter &
+$PYTHONBIN -m pip install --user -U --no-binary :all: ipython &
 
 wait
