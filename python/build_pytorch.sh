c60ad6c7c63d90b9ad8252870814b0f1a4915cf2
Kong, Zelun <zelun.kong@outlook.com>
2020-11-19 23:15:34 -0600

    python: build_pytorch.sh

--- python/build_pytorch.sh
+++ python/build_pytorch.sh
@@ -1,41 +1,113 @@
 #!/bin/bash
+# 
+# build_pytorch.sh
+# 
 
-set -e
+VERSION=1.0
 
-# BUILDDIR=/tmps
-BUILDDIR=/dev/shm
+# arguments/options
 
-# PYTHONBIN=/usr/bin/python
+PLATFORM=
+TORCH_VERSION=
 PYTHONBIN=/usr/bin/python3
-# PYTHONBIN=/usr/local/bin/python3.9
+CLEAN=0
+BUILDDIR=/tmp
+CUDA_ARCH_LIST="6.1;6.2;7.0;7.0+PTX;7.2;7.2+PTX;7.5;7.5+PTX;8.6;8.6+PTX"
+
+# functions
+
+usage() {
+    printf "build_pytorch %s\n" "$VERSION"
+    echo
+    printf "build_pytorch will build PyTorch from source.\n"
+    echo
+    printf "Usage: lfs-build -P platform -t torch_version [ -p python ] [ -C ]\n"
+    echo
+    echo "    -h, --help            display this help message and exit"
+    echo "    -v, --version         display version information and exit"
+    echo "    -P, --platform        platform (tegra, kzl-linux)"
+    echo "    -t, --torch_version   pytorch version"
+    echo "    -p, --python          python executable path"
+    echo "    -C, --clear           run \"setup.py clean\""
+    echo
+}
+
+# program start
+
+set -e -u -o pipefail
+set -x
+
+umask 0022
+
+while (( "$#" )); do
+    case "$1" in
+        -h|--help)              usage; exit 0 ;;
+        -v|--version)           printf "$VERSION"; exit 0 ;;
+        -P|--platform)          shift; PLATFORM="$1" ;;
+        -t|--torch_version)     shift; TORCH_VERSION="$1" ;;
+        -p|--python)            shift; PYTHONBIN="$1" ;;
+        -C|--clean)             CLEAN=1 ;;
+        *)                      printf "unknown option \"$1\""; exit 1 ;;
+    esac
+    shift
+done
 
 export CPPFLAGS="-D_FORTIFY_SOURCE=2"
 export CFLAGS="-march=native -O2 -pipe -fstack-protector-strong -fno-plt"
 export CXXFLAGS="-march=native -O2 -pipe -fstack-protector-strong -fno-plt"
 export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
 
-# tegra
+# common
 export BUILD_BINARY=ON
 export BUILD_TEST=OFF
 export USE_FFMPEG=ON
 export USE_GFLAGS=ON
 export USE_GLOG=ON
-export USE_NCCL=OFF
-export USE_PYTORCH_QNNPACK=OFF
-export USE_QNNPACK=OFF
-export USE_TENSORRT=ON
-export USE_XNNPACK=OFF
-export USE_ZSTD=ON
 
-export TORCH_BUILD_VERSION=1.7.0
+export TORCH_BUILD_VERSION=$TORCH_VERSION
 
-# sudo pacman -Sy --needed --noconfirm ffmpeg gflags google-glog intel-mkl nccl lapack
+if [ $PLATFORM = "kzl-linux" ]; then
+    sudo pacman -Sy --needed --noconfirm \
+        ffmpeg \
+        gflags \
+        google-glog \
+        intel-mkl \
+        nccl \
+        lapack
+elif [ $PLATFORM = "tegra" ]; then
+    BUILDDIR=/dev/shm
+    CUDA_ARCH_LIST="6.2"
+    export USE_NCCL=OFF
+    export USE_PYTORCH_QNNPACK=OFF
+    export USE_QNNPACK=OFF
+    export USE_TENSORRT=ON
+    export USE_XNNPACK=OFF
+    export USE_ZSTD=ON
 
-sudo apt update
-sudo apt install -y \
-    nvidia-jetpack libgflags-dev libgoogle-glog-dev libnuma-dev libopenblas-dev libatlas-base-dev liblapack-dev libopenmpi-dev \
-    libavcodec-dev libavformat-dev libavdevice-dev libavfilter-dev libavresample-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev \
-    zstd libzstd-dev
+    sudo apt update
+    sudo apt install -y \
+        nvidia-jetpack \
+        libgflags-dev \
+        libgoogle-glog-dev \
+        libnuma-dev \
+        libopenblas-dev \
+        libatlas-base-dev \
+        liblapack-dev \
+        libopenmpi-dev \
+        libavcodec-dev \
+        libavformat-dev \
+        libavdevice-dev \
+        libavfilter-dev \
+        libavresample-dev \
+        libavutil-dev \
+        libpostproc-dev \
+        libswresample-dev \
+        libswscale-dev \
+        zstd libzstd-dev
+else
+    printf "unknown platform \"$1\""
+    exit 1
+fi
 
 # sudo apt update
 # sudo apt install -y libassuan-dev intel-mkl-full ffmpeg \
@@ -43,27 +115,25 @@ sudo apt install -y \
 #     libavcodec-dev libavformat-dev libavdevice-dev libavfilter-dev libavresample-dev libavutil-dev libpostproc-dev libswresample-dev libswscale-dev
 
 cd $BUILDDIR
-if [ -d pytorch ]; then
-    echo "\"$BUILDDIR/pytorch\" exists"
-    exit 1
+if [ ! -d pytorch ]; then
+    git clone --recursive https://github.com/pytorch/pytorch.git
 fi
 
-git clone --recursive https://github.com/pytorch/pytorch.git
 cd pytorch
 git checkout v$TORCH_BUILD_VERSION
 git submodule update --init --recursive
 
-rm -rf .git
-
 $PYTHONBIN -m pip install --user -U --no-binary :all: ninja
 $PYTHONBIN -m pip install --user -U --no-binary :all: -r requirements.txt
-$PYTHONBIN setup.py clean
-TORCH_CUDA_ARCH_LIST="6.1" $PYTHONBIN setup.py bdist_wheel
-# TORCH_CUDA_ARCH_LIST="6.1;6.2;7.0;7.0+PTX;7.2;7.2+PTX;7.5;7.5+PTX;8.6;8.6+PTX" $PYTHONBIN setup.py bdist_wheel
+
+if (( CLEAN )); then
+    $PYTHONBIN setup.py clean
+fi
+
+TORCH_CUDA_ARCH_LIST=$CUDA_ARCH_LIST $PYTHONBIN setup.py bdist_wheel
 sudo mkdir -p /home/.repository/pip
 sudo cp dist/*.whl /home/.repository/pip
-$PYTHONBIN -m pip install -U --user /home/.repository/pip/torch-$TORCH_BUILD_VERSION*-*.whl
 
-echo "#################################"
-echo "   PyTorch build successfully.   "
-echo "#################################"
+echo "################################################################################"
+echo "  PyTorch build successfully."
+echo "################################################################################"
