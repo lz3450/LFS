387abba3dfeaf391f8aae9206046ab50dedaf8d0
Kong, Zelun <zelun.kong@outlook.com>
2021-08-16 21:00:01 -0500

    L4T: 32.6.1 and toolchain

--- L4T/init.sh
+++ L4T/init.sh
@@ -1,2 +1,3 @@
 export ROOTDIR=$(pwd)
 export PATH=$ROOTDIR/scripts:$PATH
+export CROSS_COMPILE_AARCH64_PATH=$ROOTDIR/toolchain/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu
--- L4T/scripts/jetson.sh
+++ L4T/scripts/jetson.sh
@@ -4,14 +4,12 @@
 #
 
 set -e
-set -x
+# set -x
 
 script_name="$(basename "${0}")"
 bsp_number=""
 bsp_name=""
-bsp_version="32.5.2"
-t210_bsp="jetson-210_linux_r${bsp_version}_aarch64.tbz2"
-t186_bsp="jetson_linux_r${bsp_version}_aarch64.tbz2"
+bsp_version="32.6.1"
 l4t_dir="${ROOTDIR}/${bsp_name}/Linux_for_Tegra"
 chroot_path="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
@@ -22,21 +20,31 @@ usage() {
         errno=1
     fi
 
-    echo "Usage: ${script_name} -b|--bsp <bsp_name> [-h | --help ] [-V | --verbose]"
+    echo "Usage: ${script_name} [ -h | --help ] -b|--bsp <bsp_name> [ -c | --clean ] [ -V | --verbose ]"
     echo
     echo "    -h, --help        display this help message and exit"
     echo "    -b, --bsp         specify the bsp name (t186, t210)"
+    echo "    -c, --clean       clean workspace"
     echo "    -V, --verbose     display more info"
     echo
     exit ${errno}
 }
 
 download() {
-    local name=$1
-    local link=$2
+    local bsp="${bsp_name}_linux_r${bsp_version}_aarch64.tbz2"
+    local version
 
-    if [ ! -f ${ROOTDIR}/downloads/${name} ]; then
-        wget ${link}/${name} -P ${ROOTDIR}/downloads/
+    if [ ! -f ${ROOTDIR}/downloads/${bsp} ]; then
+        wget -P ${ROOTDIR}/downloads/ -c "https://developer.nvidia.com/embedded/l4t/r${bsp_version%%.*}_release_v${bsp_version#*.}/${bsp_number}/${bsp}"
+        wget -O ${ROOTDIR}/downloads/${bsp_name}_public_sources.tbz2 -c "https://developer.nvidia.com/embedded/l4t/r${bsp_version%%.*}_release_v${bsp_version#*.}/sources/${bsp_number}/public_sources.tbz2"
+    fi
+}
+
+toolchain() {
+    local name="l4t-gcc-7-3-1-toolchain-64-bit"
+
+    if [ ! -f ${ROOTDIR}/downloads/${name}.tbz2 ]; then
+        wget -O ${ROOTDIR}/downloads/${name}.tbz2 -c "https://developer.nvidia.com/embedded/dlc/${name}"
     fi
 }
 
@@ -44,6 +52,7 @@ init_l4t() {
     if [ ! -d "${ROOTDIR}/${bsp_name}" ]; then
         mkdir -p "${ROOTDIR}"/${bsp_name}
         tar -xf "${ROOTDIR}"/downloads/${bsp_name}_linux_r${bsp_version}_aarch64.tbz2 -C "${ROOTDIR}"/${bsp_name}
+        tar -xf "${ROOTDIR}"/downloads/${bsp_name}_public_sources.tbz2 -C "${ROOTDIR}"/${bsp_name}
     fi
 
     pushd "${l4t_dir}" > /dev/null 2>&1
@@ -63,7 +72,7 @@ init_l4t() {
         echo '428a414a-de2b-4220-927d-f4bc3280592a' | sudo tee "${ROOTDIR}"/${bsp_name}/Linux_for_Tegra/bootloader/l4t-rootfs-uuid.txt_ext
         sed 's|isolcpus=1-2|isolcpus=|' -i "${ROOTDIR}"/jetson/Linux_for_Tegra/p2771-0000.conf.common
     elif [ ${bsp_number} == "t210" ]; then
-        pushd "${l4t_dir}" > /dev/null 2>&1
+        pushd "${l4t_dir}"/tools > /dev/null 2>&1
         sudo ./jetson-disk-image-creator.sh -o "${l4t_dir}"/sd_blob.img -b jetson-nano-2gb-devkit
         popd > /dev/null
     fi
@@ -82,6 +91,7 @@ while [ -n "${1}" ]; do
         ;;
     -c|--clean)
         sudo rm -rf "${ROOTDIR}"/{jetson,jetson-210}
+        exit 0
         ;;
     -V|--verbose)
         verbose=true
@@ -99,22 +109,24 @@ fi
 
 start_time=$(date +%s)
 
-echo "****************************************************************"
-echo "                Initialize Linux for Tegra                "
-echo "****************************************************************"
+echo "********************************************************************************"
+echo "    Initialize Linux for Tegra"
+echo "********************************************************************************"
 
 mkdir -p ${ROOTDIR}/downloads
 
 # L4T BSP
 # https://docs.nvidia.com/jetson/l4t/index.html#page/Tegra%20Linux%20Driver%20Package%20Development%20Guide/rootfs_custom.html#
-download ${t210_bsp} "https://developer.nvidia.com/embedded/l4t/r32_release_v5.2/t210"
-download ${t186_bsp} "https://developer.nvidia.com/embedded/l4t/r32_release_v5.2/t186"
 
 if [ ${bsp_number} == "t186" ]; then
     bsp_name="jetson"
 elif [ ${bsp_number} == "t210" ]; then
     bsp_name="jetson-210"
 fi
+
+download
+toolchain
+
 l4t_dir="${ROOTDIR}/${bsp_name}/Linux_for_Tegra"
 
 init_l4t
@@ -122,8 +134,8 @@ init_l4t
 end_time=$(date +%s)
 total_time=$((end_time-start_time))
 
-echo "****************************************************************"
-echo "                Execution time Information                "
-echo "****************************************************************"
+echo "********************************************************************************"
+echo "    Execution time Information"
+echo "********************************************************************************"
 echo "${script_name} : End time - $(date)"
 echo "${script_name} : Total time - $(date -d@${total_time} -u +%H:%M:%S)"
