a6d67e6e19a5e1373c47cfc6612c77c45f960032
Kong, Zelun <zelun.kong@outlook.com>
2022-03-19 12:26:59 -0500

    python

--- python/python_wheels.sh
+++ python/python_wheels.sh
@@ -1,5 +1,5 @@
 PIP="python3 -m pip"
-WHEEL_ARGS="wheel -w ~/wheels --no-binary :all: --no-deps"
+WHEEL_ARGS="wheel -w ~/wheels --no-binary :all:"
 INSTALL_ARGS="install -U -f ~/wheels"
 
 ${PIP} ${WHEEL_ARGS} pip
@@ -13,3 +13,6 @@ sudo ${PIP} ${INSTALL_ARGS} wheel
 
 ${PIP} ${WHEEL_ARGS} autopep8 pylint
 sudo ${PIP} ${INSTALL_ARGS} autopep8 pylint
+
+${PIP} ${WHEEL_ARGS} numpy pandas ipython jupyter
+sudo ${PIP} ${INSTALL_ARGS} numpy pandas ipython jupyter
