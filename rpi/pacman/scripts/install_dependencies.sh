3dbeb46001be41a79f18b5136519b2a37ec259a5
Kong, Zelun <zelun.kong@outlook.com>
2022-04-12 04:22:44 -0500

    rpi: pacman

---
+++ rpi/pacman/scripts/install_dependencies.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y \
+    cmake meson ninja-build autoconf m4 \
+    curl libssl-dev libcurl4-openssl-dev libassuan-dev libarchive-tools libarchive-dev libgpgme-dev fakeroot fakechroot zstd \
+    zlib1g-dev \
+    valgrind \
+    liblzma-dev \
+    swig \
+    libedit-dev \
+    rsync \
+    libelf-dev \
+    libffi-dev
