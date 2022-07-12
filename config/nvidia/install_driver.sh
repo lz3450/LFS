4ba0715715a015a49509eb884811ff9b80477ed1
Kong, Zelun <zelun.kong@outlook.com>
2022-07-12 11:48:04 -0500

    config/nvidia

--- config/nvidia/install_driver.sh
+++ config/nvidia/install_driver.sh
@@ -1,29 +1,39 @@
-# workdir=/tmp/nvidia
-# pkgname=nvidia
-# pkgver=510.54
-# source=https://us.download.nvidia.com/XFree86/Linux-x86_64/$pkgver/NVIDIA-Linux-x86_64-$pkgver.run
+workdir=/tmp/nvidia
+pkgname=nvidia
+pkgver=515.57
+source=https://us.download.nvidia.com/XFree86/Linux-x86_64/$pkgver/NVIDIA-Linux-x86_64-$pkgver.run
 
-# mkdir $workdir
-# cd $workdir
+set -e
 
-# if [ ! -f NVIDIA-Linux-x86_64-$pkgver.run ]; then
-#     wget $source
-# fi
+mkdir -p $workdir
+cd $workdir
 
-# bash NVIDIA-Linux-x86_64-$pkgver.run --extract-only
-# cd NVIDIA-Linux-x86_64-$pkgver
+if [ ! -f NVIDIA-Linux-x86_64-$pkgver.run ]; then
+    wget $source
+fi
 
-# sudo ./nvidia-installer \
-#     --accept-license \
-#     --expert \
-#     --log-file-name=$workdir/install.log \
-#     --no-precompiled-interface \
-#     --no-backup \
-#     --no-distro-scripts \
-#     --no-wine-files \
-#     --no-kernel-module-source \
-#     --no-check-for-alternate-installs \
-#     -j 8 \
-#     --force-libglx-indirect
+if [ -d NVIDIA-Linux-x86_64-$pkgver ]; then
+    sudo rm -rf NVIDIA-Linux-x86_64-$pkgver
+fi
 
-printf "%s" "blacklist nouveau" | sudo install -Dm644 /dev/stdin /etc/modprobe.d/nouveau_blacklist.conf
+if [ -d install.log ]; then
+    sudo rm -rf install.log
+fi
+
+bash NVIDIA-Linux-x86_64-$pkgver.run --extract-only
+cd NVIDIA-Linux-x86_64-$pkgver
+
+sudo ./nvidia-installer \
+    --accept-license \
+    --expert \
+    --log-file-name=$workdir/install.log \
+    --no-precompiled-interface \
+    --no-backup \
+    --no-distro-scripts \
+    --no-wine-files \
+    --no-kernel-module-source \
+    --no-check-for-alternate-installs \
+    -j 8 \
+    --force-libglx-indirect
+
+# printf "%s" "blacklist nouveau" | sudo install -Dm644 /dev/stdin /etc/modprobe.d/nouveau_blacklist.conf
