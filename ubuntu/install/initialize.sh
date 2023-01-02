027958c7e644c99862df322adf3114bc6e4da107
KZL <lz3450@outlook.com>
2023-01-02 00:56:01 -0600

    ubuntu: install scripts

--- ubuntu/install/initialize.sh
+++ ubuntu/install/initialize.sh
@@ -1,15 +1,6 @@
 #! /bin/sh
 
-cat > /etc/apt/sources.list << EOF
-deb http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
-deb-src http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
-
-deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted
-deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted
-
-deb http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
-deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
-EOF
+set -e -u
 
 apt update
 
@@ -17,14 +8,9 @@ apt install -y \
     zsh \
     wget \
     nano \
+    f2fs-tools \
     zsh-autosuggestions \
-    zsh-syntax-highlighting \
-    f2fs-tools
-wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-
-fallocate -l 16G /mnt/swapfile
-mkswap /mnt/swapfile
-chmod 600 /mnt/swapfile
+    zsh-syntax-highlighting
 
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
@@ -32,45 +18,12 @@ dpkg-reconfigure tzdata
 read -p 'hostname: ' HOSTNAME
 echo $HOSTNAME > /etc/hostname
 
-nano /etc/hosts
-
 bootctl install --esp-path=/boot/efi
-
-cat > /boot/loader/loader.conf << EOF
-timeout 3
-console-mode max
-default ubuntu
-EOF
-
-cat > /boot/loader/entries/ubuntu.conf << EOF
-title   Ubuntu 22.04
-linux   /vmlinuz
-initrd  /initrd.img
-options root=
-EOF
-
-blkid >> /boot/loader/entries/ubuntu.conf
-
-nano /boot/loader/entries/ubuntu.conf
 
 # passwd
 echo "root password:"
 passwd
 
-useradd -m -U -G wheel -s /bin/zsh kzl
+useradd -m -U -G sudo -s /bin/zsh kzl
 echo "kzl password:"
 passwd kzl
-
-# zsh
-echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' | tee -a /root/.zshrc
-echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' | tee -a /root/.zshrc
-echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' | sudo -u kzl tee -a /home/kzl/.zshrc
-echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' | sudo -u kzl tee -a /home/kzl/.zshrc
-
-# environment variables
-cat > ~/.zshenv << EOF 
-typeset -U PATH path
-path=("$HOME/.local/bin" "\$path[@]")
-export PATH
-fpath=(/usr/local/share/zsh/site-functions $fpath)
-EOF
--- ubuntu/install/install.sh
+++ ubuntu/install/install.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
+#!/bin/bash
+
+set -e -u
 
 # EFI: /boot/efi
 
@@ -9,7 +11,54 @@ done
 
 debootstrap jammy /mnt http://us.archive.ubuntu.com/ubuntu
 
+cat > /mnt/etc/apt/sources.list << EOF
+deb http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
+deb-src http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
+
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted
+deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted
+
+deb http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
+deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
+EOF
+
+fallocate -l 16G /mnt/swapfile
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+
+# boot
+mkdir -p /mnt/boot/efi/loader/entries
+
+cat > /boot/efi/loader/loader.conf << EOF
+timeout 3
+console-mode max
+default ubuntu
+EOF
+
+cat > /boot/efi/loader/entries/ubuntu.conf << EOF
+title   Ubuntu 22.04
+linux   /vmlinuz
+initrd  /initrd.img
+options root="PARTUUID=" rw
+EOF
+
+blkid >> /boot/efi/loader/entries/ubuntu.conf
+nano /boot/efi/loader/entries/ubuntu.conf
+
+# zsh
+wget -O /mnt/root/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' | tee -a /root/.zshrc
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' | tee -a /root/.zshrc
+
+# environment variables
+cat > ~/.zshenv << EOF 
+typeset -U PATH path
+path=("$HOME/.local/bin" "\$path[@]")
+export PATH
+fpath=(/usr/local/share/zsh/site-functions $fpath)
+EOF
+
 cp initialize.sh /mnt/root
-genfstab -U /mnt > /mnt/etc/fstab
+genfstab -t PARTUUID /mnt > /mnt/etc/fstab
 
 LANG=C.UTF-8 PATH=/usr/bin:/usr/sbin chroot /mnt /bin/bash
