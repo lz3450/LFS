1a2a859c9b4f3ccc450e6bfa49759a621c70fe28
KZL <lz3450@outlook.com>
2023-01-03 10:38:39 -0600

    ubuntu/install

--- ubuntu/install/install-software.sh
+++ ubuntu/install/install-software.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# 
+#
 # install-software.sh
-# 
+#
 
 sudo apt update
 sudo apt upgrade -y
 sudo apt install software-properties-common apt-transport-https wget
 
 ### microsoft
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo install -o root -g root -m 644 /dev/stdin /etc/apt/trusted.gpg.d/microsoft.gpg
 # sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-rm microsoft.gpg
