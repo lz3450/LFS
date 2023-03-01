b3794427557da63e2ca0f3277e7bc4cd68628f45
KZL <lz3450@outlook.com>
2023-03-01 12:39:26 -0600

    update config: zsh

--- config/zsh/oh-my-zsh.sh
+++ config/zsh/oh-my-zsh.sh
@@ -2,12 +2,12 @@
 
 set -e -u
 
-cd
+cd ~
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd .oh-my-zsh/custom/plugins
+cd ~/.oh-my-zsh/custom/plugins
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git
 cd
 sed -e "/^ZSH_THEME/s/\"robbyrussell\"/random/" \
     -e "/^plugins=/s/git/archlinux git vscode python tmux zsh-autosuggestions zsh-syntax-highlighting/" \
-    -i .zshrc
+    -i ~/.zshrc
