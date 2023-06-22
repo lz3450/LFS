7e3e4c470ea6ee64397303794608a1b65d90f8b6
KZL <lz3450@outlook.com>
2023-06-22 12:04:01 -0500

    update scripts: ubuntu

--- ubuntu/pacman/scripts/build_pacman.sh
+++ ubuntu/pacman/scripts/build_pacman.sh
@@ -4,7 +4,22 @@ set -e -u
 # set -x
 
 BUILDDIR=/tmp
-pacmanver=6.0.1
+pacmanver=$(curl -L -A 'Mozilla/5.0' --stderr - https://gitlab.archlinux.org/pacman/pacman/-/tags | grep -Po '(?<=/pacman/-/tags/v)\d\.\d\.\d' | head -1)
+
+sudo apt update
+sudo apt install -y \
+    cmake meson ninja-build autoconf m4 pkg-config\
+    curl libssl-dev libcurl4-openssl-dev libassuan-dev libarchive-tools libarchive-dev libgpgme-dev fakeroot fakechroot zstd \
+    zlib1g-dev \
+    valgrind \
+    liblzma-dev \
+    swig \
+    libedit-dev \
+    rsync \
+    libelf-dev \
+    libffi-dev \
+    flex bison \
+    python3-setuptools
 
 cd $BUILDDIR
 if [ ! -f pacman-$pacmanver.tar.gz ]; then
@@ -19,38 +34,40 @@ tar -xf pacman-$pacmanver.tar.xz
 cd pacman-$pacmanver
 
 meson setup \
-  --prefix /usr/local \
-  --libexecdir lib \
-  --sbindir bin \
-  --sysconfdir etc \
-  --auto-features enabled \
-  --buildtype plain \
-  --wrap-mode nodownload \
-  -D b_lto=true \
-  -D b_pie=true \
-  -D buildstatic=false \
-  -D doc=disabled \
-  -D doxygen=disabled \
-  -D ldconfig=/usr/bin/ldconfig \
-  -D pkg-ext='.pkg.tar.zst' \
-  -D scriptlet-shell=/usr/bin/bash \
-  -D src-ext='.src.tar.zst' \
-  build
+    --prefix /usr/local \
+    --sysconfdir etc \
+    --optimization 2 \
+    -D b_lto=true \
+    -D b_pie=true \
+    -D buildstatic=false \
+    -D crypto=openssl \
+    -D curl=enabled \
+    -D doc=disabled \
+    -D doxygen=disabled \
+    -D file-seccomp=enabled \
+    -D gpgme=enabled \
+    -D ldconfig=/usr/bin/ldconfig \
+    -D pkg-ext='.pkg.tar.zst' \
+    -D scriptlet-shell=/usr/bin/bash \
+    -D src-ext='.src.tar.zst' \
+    -D use-git-version=true \
+    build
 
-meson compile -C build
+# meson compile -C build
+ninja -C build
 meson test -C build || :
 sudo meson install -C build
 
-sudo sed -e '/^CHOST=/s/x86_64-pc-linux-gnu/x86_64-kzl-linux-gnu/' \
+sudo sed -e '/^CHOST=/s/x86_64-pc-linux-gnu/x86_64-linux-gnu/' \
     -e '/^#CFLAGS=/c\CFLAGS="-march=native -O2 -pipe -fno-plt -fexceptions -fstack-clash-protection -fcf-protection -Wp,-D_FORTIFY_SOURCE=2"' \
     -e '/^#CXXFLAGS=/c\CXXFLAGS="-march=native -O2 -pipe -fno-plt -fexceptions -fstack-clash-protection -fcf-protection -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS"' \
     -e '/^#LDFLAGS=/c\LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"' \
     -e '/^#RUSTFLAGS=/c\RUSTFLAGS="-C opt-level=2"' \
-    -e '/^#MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' \
+    -e '/^#MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc) -O"' \
     -e '/^#DEBUG_CFLAGS=/c\DEBUG_CFLAGS="-g -fvar-tracking-assignments"' \
     -e '/^#DEBUG_CXXFLAGS=/c\DEBUG_CXXFLAGS="-g -fvar-tracking-assignments"' \
     -e '/^#DEBUG_RUSTFLAGS=/c\DEBUG_RUSTFLAGS="-C debuginfo=2"' \
-    -e '/^#BUILDDIR=/c\BUILDDIR=/tmp/makepkg' \
+    -e '/^#BUILDDIR=/c\BUILDDIR="$HOME/makepkg/build"' \
     -e '/^INTEGRITY_CHECK=/s/(.*)/(sha256)/' \
     -e '/^#PKGDEST=/c\PKGDEST="$HOME/makepkg/packages"' \
     -e '/^#SRCDEST=/c\SRCDEST="$HOME/makepkg/sources"' \
