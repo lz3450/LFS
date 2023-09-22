e1ab5c4841b5d7f361b487a6c1c1bbbcc0694383
KZL <lz3450@outlook.com>
2023-09-22 01:04:13 -0500

    update config

--- config/aria2/apply.sh
+++ config/aria2/apply.sh
@@ -9,16 +9,17 @@ mkdir -p ~/.config/systemd/user/
 cp aria2.conf ~/.config/aria2/aria2.conf
 cp aria2-rpc.conf ~/.config/aria2/aria2-rpc.conf
 cp aria2cd.service ~/.config/systemd/user/
-cp aria2-webui.service ~/.config/systemd/user/
 touch $ARIA2_HOME/aria2.log
 touch $ARIA2_HOME/aria2.session
 touch $ARIA2_HOME/aria2-rpc.log
 touch $ARIA2_HOME/aria2-rpc.session
 
-if [ ! -d $ARIA2_HOME/webui-aria2 ]; then
-    cd $ARIA2_HOME
-    git clone https://github.com/ziahamza/webui-aria2.git
-else
-    cd $ARIA2_HOME/webui-aria2
-    git pull
-fi
+# # aria2-webui
+# cp aria2-webui.service ~/.config/systemd/user/
+# if [ ! -d $ARIA2_HOME/webui-aria2 ]; then
+#     cd $ARIA2_HOME
+#     git clone https://github.com/ziahamza/webui-aria2.git
+# else
+#     cd $ARIA2_HOME/webui-aria2
+#     git pull
+# fi
