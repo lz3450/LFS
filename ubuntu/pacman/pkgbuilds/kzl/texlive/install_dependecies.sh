9e695ad8967e0fc5b5aa466496baccb2550bb048
KZL <lz3450@outlook.com>
2024-01-17 15:06:39 -0600

    ubuntu: pkgbuilds

---
+++ ubuntu/pacman/pkgbuilds/kzl/texlive/install_dependecies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install \
+    libcairo2-dev \
+    fontconfig \
+    libfreetype-dev \
+    libgd-dev \
+    ghostscript \
+    libgmp-dev \
+    libgraphite2-dev \
+    libharfbuzz-dev \
+    libicu-dev \
+    libpaper-dev \
+    libpng-dev \
+    libmpfr-dev \
+    libpixman-1-dev \
+    libzzip-dev
