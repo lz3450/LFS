9e972556a0a0ecb2d5f250b6740e28aebbc83e2e
KZL <lz3450@outlook.com>
2024-02-04 01:37:40 -0600

    kzl-linux: pkgbuild

---
+++ ubuntu/pacman/pkgbuilds/kzl/python/install_dependecies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install \
+    libexpat1-dev \
+    libgdbm-dev \
+    libgdbm-compat-dev \
+    libdb5.3 \
+    libedit-dev \
+    libffi-dev \
+    libnsl-dev \
+    libsqlite3-dev \
+    tcl-dev \
+    tk-dev \
+    libmpdec-dev
