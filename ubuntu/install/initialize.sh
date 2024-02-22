c139adb4ad236e6a9bcbe2e7935bdff4cbda88a2
KZL <lz3450@outlook.com>
2024-02-22 13:42:46 -0600

    ubuntu: install

--- ubuntu/install/initialize.sh
+++ ubuntu/install/initialize.sh
@@ -1,23 +1,48 @@
 #! /bin/sh
 
-set -e -u
+set -e
+# set -x
+
+# package
+tee /etc/apt/sources.list << EOF
+deb http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
+deb-src http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
+
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted
+deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted
+
+deb http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
+deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
+EOF
 
 apt update
-
+apt upgrade -y
 apt install -y \
-    zsh \
+    sudo \
+    f2fs-tools \
+    wpasupplicant \
     wget curl \
     nano \
+    openssh-server \
+    bash-completion \
+    zsh \
     zsh-autosuggestions \
     zsh-syntax-highlighting
 
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
 
+###############################################################################
+
+set +e
+
 read -p 'hostname: ' HOSTNAME
 echo $HOSTNAME > /etc/hostname
 
-bootctl install --esp-path=/boot/efi
+# grml-zsh-config
+wget -O /root/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> /root/.zshrc
+echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /root/.zshrc
 
 # passwd
 echo "root password:"
@@ -28,7 +53,14 @@ echo "kzl password:"
 passwd kzl
 
 # network
-cat > /etc/systemd/network/ethernet.network << EOF
+tee /etc/systemd/network/wireless.network << EOF
+[Match]
+Name=wl*
+
+[Network]
+DHCP=yes
+EOF
+tee /etc/systemd/network/ethernet.network << EOF
 [Match]
 Name=en*
 Name=eth*
@@ -36,9 +68,37 @@ Name=eth*
 [Network]
 DHCP=yes
 EOF
+tee /etc/wpa_supplicant/wpa_supplicant-wlo1.conf << EOF
+network={
+	ssid="LuckySKZLJ"
+	psk=a8ba39d6dc7bc6e4984dcc45a386719fe42b5876dc4fd56c516a521615ee981c
+}
+network={
+	ssid="S3Lab"
+	psk=9dfacd4f5b26c7bfde13a184acb4b202eba5b2870cb2d6dccd10ac53012d0706
+}
+EOF
 
 cat > /etc/netplan/network.yaml << EOF
 network:
   version: 2
   renderer: NetworkManager
+EOF
+
+# boot
+bootctl install --esp-path=/boot/efi
+
+mkdir -p /boot/efi/loader/entries
+
+tee /boot/efi/loader/loader.conf << EOF
+timeout 3
+console-mode max
+default ubuntu.conf
+EOF
+
+tee /boot/efi/loader/entries/ubuntu.conf << EOF
+title   Ubuntu 22.04
+linux   /vmlinuz
+initrd  /initrd.img
+options root=PARTUUID="" rw
 EOF
--- ubuntu/install/install.sh
+++ ubuntu/install/install.sh
@@ -2,8 +2,6 @@
 
 set -e -u
 
-# EFI: /boot/efi
-
 debootstrap --arch="amd64" jammy /mnt http://us.archive.ubuntu.com/ubuntu
 
 for fs in dev sys proc run; do
@@ -11,60 +9,22 @@ for fs in dev sys proc run; do
     mount --make-rslave /mnt/$fs
 done
 
-cat > /mnt/etc/apt/sources.list << EOF
-deb http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
-deb-src http://us.archive.ubuntu.com/ubuntu jammy main restricted universe
-
-deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted
-deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted
-
-deb http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
-deb-src http://us.archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe
-EOF
-
 fallocate -l 16G /mnt/swapfile
 chmod 600 /mnt/swapfile
 mkswap /mnt/swapfile
 
+cp initialize.sh /mnt/root
+LANG=C.UTF-8 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin chroot /mnt /bin/bash
+
 # boot
-mkdir -p /mnt/boot/efi/loader/entries
-
-cat > /mnt/boot/efi/loader/loader.conf << EOF
-timeout 3
-console-mode max
-default ubuntu.conf
-EOF
-
-cat > /mnt/boot/efi/loader/entries/ubuntu.conf << EOF
-title   Ubuntu 22.04
-linux   /vmlinuz
-initrd  /initrd.img
-options root=PARTUUID="" rw
-EOF
-
+# EFI: /boot/efi
 blkid >> /mnt/boot/efi/loader/entries/ubuntu.conf
 nano /mnt/boot/efi/loader/entries/ubuntu.conf
 
-# zsh
-wget -O /mnt/root/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' | tee -a /mnt/root/.zshrc
-echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' | tee -a /mnt/root/.zshrc
-
-# environment variables
-cat > /mnt/root/.zshenv << EOF
-typeset -U PATH path
-path=("\$HOME/.local/bin" "\$path[@]")
-export PATH
-fpath=(/usr/local/share/zsh/site-functions \$fpath)
-EOF
-
-cp initialize.sh /mnt/root
-
+# fstab
 if [ -f "/usr/bin/genfstab" ]; then
     genfstab -t PARTUUID /mnt > /mnt/etc/fstab
 else
     blkid > /mnt/etc/fstab
 fi
 nano /mnt/etc/fstab
-
-LANG=C.UTF-8 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin chroot /mnt /bin/bash
