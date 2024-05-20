3f54dd0d2d74a63e4d58a585880337bd57e0f7dd
KZL <lz3450@outlook.com>
2024-05-20 11:58:16 -0500

    ubuntu: scripts

--- ubuntu/scripts/unlock_remote_desktop.sh
+++ ubuntu/scripts/unlock_remote_desktop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
-pkill -f -u kzl gnome-keyring-daemon
-echo -n 'skzlj999' | gnome-keyring-daemon --unlock
+# pkill -f -u kzl gnome-keyring-daemon
+read -r -s -p "Enter password: " password
+echo
+echo -n "$password" | gnome-keyring-daemon -r -d --unlock
 systemctl --user restart gnome-remote-desktop.service
