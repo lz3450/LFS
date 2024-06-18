25ac40b6ac257f6c6a8fdbf9b39ce2091376b3a3
KZL <lz3450@outlook.com>
2024-06-17 21:54:12 -0600

    rpi: mkimg

--- rpi/scripts/performance.sh
+++ rpi/scripts/performance.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
