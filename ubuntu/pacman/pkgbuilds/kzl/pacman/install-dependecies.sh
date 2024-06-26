e4f088ce7dc241771ca33d0dd19c5ee88cdc0698
KZL <lz3450@outlook.com>
2024-06-26 05:05:26 +0000

    ubuntu: pkgbuilds

--- ubuntu/pacman/pkgbuilds/kzl/pacman/install-dependecies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt install \
-    libarchive-tools \
-    libcurl4-openssl-dev \
-    libgpgme-dev
---
+++ ubuntu/pacman/pkgbuilds/kzl/pacman/install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install \
+    cmake \
+    meson ninja-build \
+    pkg-config \
+    bash-completion \
+    libarchive-dev \
+    libarchive-tools \
+    libcurl4-openssl-dev \
+    libgpgme-dev \
+    libssl-dev \
+    fakeroot \
+    fakechroot
