5653765a44ad55d62507e880aa25dda23dc0758f
KZL <lz3450@outlook.com>
2024-09-22 12:54:06 -0500

    ubuntu: pacman

--- ubuntu/pacman/pkgbuilds/kzl/linux/update-kernel.sh
+++ ubuntu/pacman/pkgbuilds/kzl/linux/update-kernel.sh
@@ -7,8 +7,16 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+if [ -n "$BASH_VERSION" ]; then
+  export ROOTDIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1; pwd -P)"
+elif [ -n "$ZSH_VERSION" ]; then
+  export ROOTDIR="$(cd -- "$(dirname "${(%):-%x}")" >/dev/null 2>&1; pwd -P)"
+else
+  echo "Unsupported shell"
+fi
+
 update-initramfs -k all -c
 
-cp -v initrd.img efi/
-cp -v vmlinuz efi/
-cp -v initrd.img-@pkgver@-KZL efi/initrd-KZL.img
+cp -v "$ROOTDIR"/initrd.img "$ROOTDIR"/efi/
+cp -v "$ROOTDIR"/vmlinuz "$ROOTDIR"/efi/
+cp -v "$ROOTDIR"/initrd.img-@pkgver@-KZL "$ROOTDIR"/efi/initrd-KZL.img
