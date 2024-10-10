8cb3cf1cba1c2db6fa312c378fef85c85bc30781
KZL <lz3450@outlook.com>
2024-10-09 22:02:00 -0500

    config: ohmyzsh

--- config/zsh/oh-my-zsh.sh
+++ config/zsh/oh-my-zsh.sh
@@ -5,41 +5,53 @@ set -u
 
 ohmyzsh_dir="$HOME/.oh-my-zsh"
 plugin_rootdir="$HOME/.oh-my-zsh/custom/plugins"
-
-if [ -d "$ohmyzsh_dir" ]; then
-    rm -rf "$ohmyzsh_dir"
-fi
-
-export RUNZSH="no"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-tmpdir=/tmp/zsh-syntax-highlighting
-rm -rf "$tmpdir"
-git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git "$tmpdir"
-pushd "$tmpdir"
-mkdir -p "$plugin_rootdir"/zsh-syntax-highlighting
-make
-make \
-    PREFIX="$plugin_rootdir"/zsh-syntax-highlighting/ \
-    SHARE_DIR="$plugin_rootdir"/zsh-syntax-highlighting/ \
-    DOC_DIR="$plugin_rootdir"/zsh-syntax-highlighting/doc \
-    install
-install -Dm644 zsh-syntax-highlighting.plugin.zsh -t "$plugin_rootdir"/zsh-syntax-highlighting/
-popd
-rm -rf "$tmpdir"
+log_dir="$HOME/log"
 
 
-tmpdir=/tmp/zsh-autosuggestions
-rm -rf "$tmpdir"
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git "$tmpdir"
-pushd "$tmpdir"
-mkdir -p "$plugin_rootdir"/zsh-autosuggestions
-make
-install -Dm644 zsh-autosuggestions{,.plugin}.zsh -t "$plugin_rootdir"/zsh-autosuggestions/
-popd
-rm -rf "$tmpdir"
+install_ohmyzsh() {
+    if [ -d "$ohmyzsh_dir" ]; then
+        rm -vrf "$ohmyzsh_dir"
+    fi
 
-rm -rf "$plugin_rootdir"/zsh-syntax-highlighting/doc
+    export RUNZSH="no"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_zsh-syntax-highlighting() {
+    tmpdir=/tmp/zsh-syntax-highlighting
+    rm -vrf "$tmpdir"
+    git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git "$tmpdir"
+    pushd "$tmpdir"
+    mkdir -p "$plugin_rootdir"/zsh-syntax-highlighting
+    make
+    make \
+        PREFIX="$plugin_rootdir"/zsh-syntax-highlighting/ \
+        SHARE_DIR="$plugin_rootdir"/zsh-syntax-highlighting/ \
+        DOC_DIR="$plugin_rootdir"/zsh-syntax-highlighting/doc \
+        install
+    install -Dm644 zsh-syntax-highlighting.plugin.zsh -t "$plugin_rootdir"/zsh-syntax-highlighting/
+    popd
+    rm -vrf "$tmpdir"
+}
+
+install_zsh-autosuggestions() {
+    tmpdir=/tmp/zsh-autosuggestions
+    rm -vrf "$tmpdir"
+    git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git "$tmpdir"
+    pushd "$tmpdir"
+    mkdir -p "$plugin_rootdir"/zsh-autosuggestions
+    make
+    install -Dm644 zsh-autosuggestions{,.plugin}.zsh -t "$plugin_rootdir"/zsh-autosuggestions/
+    popd
+    rm -vrf "$tmpdir"
+    rm -vrf "$plugin_rootdir"/zsh-syntax-highlighting/doc
+}
+
+################################################################################
+mkdir -p "$log_dir"
+install_ohmyzsh &> "$log_dir/oh-my-zsh.log"
+install_zsh-syntax-highlighting &> "$log_dir/zsh-syntax-highlighting.log"
+install_zsh-autosuggestions &> "$log_dir/zsh-autosuggestions.log"
 
 cp ~/.zshrc .zshrc.orig
 ln -srfv .zshrc ~
