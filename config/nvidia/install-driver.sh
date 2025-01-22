47d2987b72644ff30c1b7bdde1ef298f1ad7111b
KZL <lz3450@outlook.com>
2025-01-22 14:51:57 -0600

    config: nvidia

--- config/nvidia/install-driver.sh
+++ config/nvidia/install-driver.sh
@@ -1,23 +1,28 @@
+#!/usr/bin/bash
+#
+# install-driver.sh
+#
+
 workdir=/tmp/nvidia
 pkgname=nvidia
-pkgver=550.120
+pkgver=565.77
 source=https://us.download.nvidia.com/XFree86/Linux-x86_64/$pkgver/NVIDIA-Linux-x86_64-$pkgver.run
 
 set -e
 
-if [ ! -f $HOME/Downloads/NVIDIA-Linux-x86_64-$pkgver.run ]; then
-    wget -P $HOME/Downloads $source
+if [[ ! -f "$HOME/Downloads/NVIDIA-Linux-x86_64-$pkgver.run" ]]; then
+    wget -P "$HOME/Downloads" "$source"
 fi
 
-sudo rm -rf $workdir
-bash $HOME/Downloads/NVIDIA-Linux-x86_64-$pkgver.run --extract-only --target $workdir
-cd $workdir
+sudo rm -rf "$workdir"
+bash "$HOME/Downloads/NVIDIA-Linux-x86_64-$pkgver.run" --extract-only --target "$workdir"
+cd "$workdir"
 
 sudo ./nvidia-installer \
     --accept-license \
     --expert \
     --no-install-compat32-libs \
-    --log-file-name=$HOME/nvidia-driver-install.log \
+    --log-file-name="$HOME"/log/nvidia-driver-install.log \
     --run-nvidia-xconfig \
     --no-distro-scripts \
     --no-wine-files \
