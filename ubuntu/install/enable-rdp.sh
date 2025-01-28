ba71a33588f3bb013dd3f68bc979b3e86301759a
KZL <lz3450@outlook.com>
2025-01-28 02:29:31 -0600

    ubuntu: install

--- ubuntu/install/enable-rdp.sh
+++ ubuntu/install/enable-rdp.sh
@@ -5,6 +5,20 @@
 
 set -e
 
+CERT_DIR=$HOME/.local/share/gnome-remote-desktop/certificate
+KEY="$CERT_DIR"/grd-tls.key
+CSR="$CERT_DIR"/grd-tls.csr
+CRT="$CERT_DIR"/grd-tls.crt
+
+if [[ ! -d  "$CERT_DIR" ]]; then
+    mkdir -p "$CERT_DIR"
+    openssl genrsa -out "$CERT_DIR"/grd-tls.key 4096
+    openssl req -new -key "$KEY" -out "$CERT_DIR"/grd-tls.csr
+    openssl x509 -req -days 365 -signkey "$KEY" -in "$CSR" -out "$CRT"
+fi
+
+grdctl rdp set-tls-cert "$CRT"
+grdctl rdp set-tls-key "$KEY"
 grdctl rdp enable
 grdctl rdp disable-view-only
 read -r -s -p "Enter rdp password: " password
