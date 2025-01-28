1cfd42c437687104f95e055202d80ed08e43dba8
KZL <lz3450@outlook.com>
2025-01-28 14:15:36 -0600

    ubuntu: pkgbuilds

--- ubuntu/pacman/pkgbuilds/ubuntu/linux/update-kernel.sh
+++ ubuntu/pacman/pkgbuilds/ubuntu/linux/update-kernel.sh
@@ -7,16 +7,17 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-if [ -n "$BASH_VERSION" ]; then
-  export ROOTDIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1; pwd -P)"
-elif [ -n "$ZSH_VERSION" ]; then
-  export ROOTDIR="$(cd -- "$(dirname "${(%):-%x}")" >/dev/null 2>&1; pwd -P)"
+if [[ -n "$BASH_VERSION" ]]; then
+  export BOOT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1; pwd -P)"
+elif [[ -n "$ZSH_VERSION" ]]; then
+  export BOOT_DIR="$(cd -- "$(dirname "${(%):-%x}")" > /dev/null 2>&1; pwd -P)"
 else
   echo "Unsupported shell"
 fi
 
 update-initramfs -k all -c
 
-cp -v "$ROOTDIR"/initrd.img "$ROOTDIR"/efi/
-cp -v "$ROOTDIR"/vmlinuz "$ROOTDIR"/efi/
-cp -v "$ROOTDIR"/initrd.img-@pkgver@-KZL "$ROOTDIR"/efi/initrd-KZL.img
+cp -v "$BOOT_DIR"/initrd.img "$BOOT_DIR"/efi/
+cp -v "$BOOT_DIR"/vmlinuz "$BOOT_DIR"/efi/
+cp -v "$BOOT_DIR"/initrd.img-@pkgver@-KZL "$BOOT_DIR"/efi/initrd-KZL.img
+cp -v "$BOOT_DIR"/vmlinuz-6.12.11-KZL "$BOOT_DIR"/efi/vmlinuz-KZL
