48847274d495a8eec52cbcb31c511580b64d8342
KZL <lz3450@outlook.com>
2025-01-29 14:53:30 -0600

    ubuntu: install

--- ubuntu/install/enable-rdp.sh
+++ ubuntu/install/enable-rdp.sh
@@ -5,10 +5,15 @@
 
 set -e
 
-CERT_DIR=$HOME/.local/share/gnome-remote-desktop/certificate
+CERT_DIR=$HOME/.config/gnome-remote-desktop/certificates
 KEY="$CERT_DIR"/grd-tls.key
 CSR="$CERT_DIR"/grd-tls.csr
 CRT="$CERT_DIR"/grd-tls.crt
+
+sudo nano /etc/gdm3/custom.conf
+sudo systemctl disable gnome-remote-desktop.service
+systemctl --user enable gnome-remote-desktop.service
+systemctl --user start gnome-remote-desktop.service
 
 if [[ ! -d  "$CERT_DIR" ]]; then
     mkdir -p "$CERT_DIR"
@@ -21,6 +26,8 @@ grdctl rdp set-tls-cert "$CRT"
 grdctl rdp set-tls-key "$KEY"
 grdctl rdp enable
 grdctl rdp disable-view-only
+
+./../scripts/unlock_remote_desktop.sh
 read -r -s -p "Enter rdp password: " password
 grdctl rdp set-credentials kzl "$password"
 grdctl status --show-credentials
