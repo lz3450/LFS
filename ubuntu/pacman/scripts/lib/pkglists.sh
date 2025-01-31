bac6191ea6e4bf28898d0beb12866cbcc231429f
KZL <lz3450@outlook.com>
2025-01-31 15:17:20 -0600

    ubuntu: pacman scripts

---
+++ ubuntu/pacman/scripts/lib/pkglists.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+#
+# pkglist.sh
+#
+
+ubuntu_pkgs=(
+    pacman
+    ksmbd-tools
+    kexec-tools
+    python
+    llvm-project
+    debootstrap
+    dracut
+    rust
+    texlive
+)
