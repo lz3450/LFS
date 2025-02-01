597ba8be49433af3af313985c292d5e809a08ac7
KZL <lz3450@outlook.com>
2025-01-31 18:42:26 -0600

    ubuntu: pacman scripts

--- ubuntu/pacman/scripts/lib/pkglists.sh
+++ ubuntu/pacman/scripts/lib/pkglists.sh
@@ -3,15 +3,18 @@
 # pkglist.sh
 #
 
-ubuntu_pkgs=(
+ubuntu_stage1_pkgs=(
     pacman
     ksmbd-tools
     kexec-tools
     python
     debootstrap
     dracut
+    texlive
     llvm-project
+)
+
+ubuntu_stage2_pkgs=(
     llvm-project
     rust
-    texlive
 )
