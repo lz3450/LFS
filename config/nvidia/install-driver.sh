3ad1f4f52137d1091132d9931dea465d75ccea3e
KZL <lz3450@outlook.com>
2025-03-18 10:25:05 -0500

    config: nvidia

--- config/nvidia/install-driver.sh
+++ config/nvidia/install-driver.sh
@@ -4,37 +4,55 @@
 #
 
 set -e
+set -o pipefail
+set -u
+# set -x
 
-workdir=/tmp/nvidia
-pkgname=nvidia
-pkgver=565.77
-source=https://us.download.nvidia.com/XFree86/Linux-x86_64/$pkgver/NVIDIA-Linux-x86_64-$pkgver.run
-runfile="$HOME/Downloads/NVIDIA-Linux-x86_64-$pkgver.run"
+umask 0022
 
-if [[ ! -f "$runfile" ]]; then
-    wget -P "$HOME/Downloads" "$source"
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+################################################################################
+
+WORKDIR=/tmp/nvidia
+DOWNLOADS_DIR="$HOME/Downloads"
+LOG_FILE="$SCRIPT_DIR/nvidia-driver-install.log"
+
+DRIVER_VERSION=565.77
+RUNFILE=NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
+
+################################################################################
+
+if [[ ! -f "$DOWNLOADS_DIR/$RUNFILE" ]]; then
+    wget -P "$DOWNLOADS_DIR" https://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/$RUNFILE
 fi
 
-sudo rm -rf "$workdir"
-bash "$runfile" -A > driver-help.txt
-bash "$runfile" --extract-only --target "$workdir"
+sudo rm -rf "$WORKDIR"
+bash "$DOWNLOADS_DIR/$RUNFILE" -A > driver-help.txt
+bash "$DOWNLOADS_DIR/$RUNFILE" --extract-only --target "$WORKDIR"
 
-cd "$workdir"
+cd "$WORKDIR"
 sudo ./nvidia-installer \
     --accept-license \
     --expert \
-    --log-file-name="$HOME"/log/nvidia-driver-install.log \
-    --kernel-name=$(uname -r) \
+    --log-file-name="$LOG_FILE" \
+    --kernel-name="$(uname -r)" \
     --no-backup \
     --disable-nouveau \
     --no-distro-scripts \
+    --no-opengl-files \
     --no-wine-files \
     --no-dkms \
     --no-check-for-alternate-installs \
-    --concurrency-level=$(nproc) \
+    --concurrency-level="$(nproc)" \
     --install-libglvnd \
     --systemd \
     --no-rebuild-initramfs
 
 sudo rm -vf /usr/lib/modprobe.d/nvidia-installer-disable-nouveau.conf
 echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia.conf
+
+sudo chown "$(id -u):$(id -g)" "$LOG_FILE"
+sudo chown "$(id -u):$(id -g)" "$SCRIPT_DIR/nvidia-uninstall.log"
