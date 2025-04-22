a24fce634990020b919a8dbbca66b8c065740722
KZL <lz3450@outlook.com>
2025-04-22 14:13:27 -0500

    ubuntu: pkgbuilds

---
+++ ubuntu/pacman/pkgbuilds/24.04/linux/install-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y \
+    bc \
+    bison \
+    flex \
+    libelf-dev \
+    libncurses-dev \
+    libssl-dev \
+    rsync
---
+++ ubuntu/pacman/pkgbuilds/24.04/python/install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pkgs=(
+    libbz2-dev
+    libedit-dev
+    libffi-dev
+    libgdbm-compat-dev
+    libgdbm-dev
+    libncurses-dev
+    libsqlite3-dev
+    tcl-dev
+    tk-dev
+)
+
+sudo apt-get update
+sudo apt-get install -y "${pkgs[@]}"
