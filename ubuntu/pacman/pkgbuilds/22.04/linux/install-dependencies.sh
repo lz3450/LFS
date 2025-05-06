a5f97e567ef1d4af0768d8a297a821fcce8274e5
KZL <lz3450@outlook.com>
2025-05-06 00:16:05 -0500

    ubuntu: pkgbuilds - apply new structure

--- ubuntu/pacman/pkgbuilds/22.04/linux/install-dependencies.sh
+++ ubuntu/pacman/pkgbuilds/22.04/linux/install-dependencies.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install -y \
-    bc \
-    bison \
-    flex \
-    libelf-dev \
-    libncurses-dev \
-    libssl-dev \
-    rsync
+../../ubuntu/linux/install-dependencies.sh
--- ubuntu/pacman/pkgbuilds/22.04/python/install-dependencies.sh
+++ ubuntu/pacman/pkgbuilds/22.04/python/install-dependencies.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-pkgs=(
-    libbz2-dev
-    libedit-dev
-    libffi-dev
-    libgdbm-compat-dev
-    libgdbm-dev
-    libncurses-dev
-    libsqlite3-dev
-    tcl-dev
-    tk-dev
-)
-
-sudo apt-get update
-sudo apt-get install -y "${pkgs[@]}"
+../../ubuntu/python/install-dependencies.sh
