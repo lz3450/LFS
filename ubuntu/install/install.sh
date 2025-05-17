5a164c8d1a25427c1494ff37847d4dca2a4ec057
KZL <lz3450@outlook.com>
2025-05-17 18:42:49 -0500

    ubuntu: install

--- ubuntu/install/install.sh
+++ ubuntu/install/install.sh
@@ -35,6 +35,10 @@ noble_deb_pkgs=(
     systemd-boot
     systemd-resolved
 )
+plucky_deb_pkgs=(
+    systemd-boot
+    systemd-resolved
+)
 exclude_pkgs=(
     ubuntu-minimal
     ubuntu-pro-client
@@ -51,6 +55,7 @@ fi
 case "$1" in
     noble)      debootstrap_suite="noble" ;;
     jammy)      debootstrap_suite="jammy" ;;
+    plucky)     debootstrap_suite="plucky" ;;
     *)          echo "Unknown suite \"$1\"" && exit 1
 esac
 
@@ -59,6 +64,8 @@ if [[ "$debootstrap_suite" == "jammy" ]]; then
     deb_pkgs=("${common_deb_pkgs[@]}")
 elif [[ "$debootstrap_suite" == "noble" ]]; then
     deb_pkgs=("${common_deb_pkgs[@]}" "${noble_deb_pkgs[@]}")
+elif [[ "$debootstrap_suite" == "plucky" ]]; then
+    deb_pkgs=("${common_deb_pkgs[@]}" "${plucky_deb_pkgs[@]}")
 fi
 debootstrap \
     --arch="amd64" \
