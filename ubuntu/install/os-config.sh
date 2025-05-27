c3e09d91dcdb03a1b9090be764cb693b9c14dd34
KZL <lz3450@outlook.com>
2025-05-27 18:03:17 -0500

    ubuntu/install/os-config.sh: set etc/environment

--- ubuntu/install/os-config.sh
+++ ubuntu/install/os-config.sh
@@ -24,7 +24,7 @@ dpkg --get-selections | awk '{print $1}' | sed -e '/^linux-image-.+/d' -e '/^lin
 set +e
 
 # etc/environment
-echo 'PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"' > /etc/environment
+echo 'PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/opt/bin"' > /etc/environment
 
 # password
 echo "root password:"
