e48cd10a84f6980e9b14f1e8012acf86b245eb26
KZL <lz3450@outlook.com>
2025-06-11 02:39:54 -0500

    bash: move lib together

---
+++ bash/lib/chroot.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#
+# chroot.sh
+#
+
+################################################################################
+
+if [[ -n "${__CHROOT__:-}" ]]; then
+    return
+fi
+
+declare -i __CHROOT__=1
+
+################################################################################
+
+### libraries
+LIBDIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1; pwd -P)"
+. "$LIBDIR"/log.sh
+. "$LIBDIR"/utils.sh
+
+### checks
+check_root
+
+### constants & variables
+CHROOT_PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
+chroot_active_mounts=()
+chroot_dir=""
+
+### functions
+chroot_add_mount() {
+    mount -v "$@"
+    chroot_active_mounts=(${@: -1} "${chroot_active_mounts[@]}")
+}
+
+_resolve_link() {
+    local _target="$1"
+    local _rootfs="${2:-}"
+
+    while [[ -L "$_target" ]]; do
+        _target=$(readlink -m "$_target")
+        # If a root was given, make sure the target is under it.
+        # Make sure to strip any leading slash from target first.
+        if [[ -n "$_rootfs" && "$_target" != "$_rootfs"* ]]; then
+            _target="${_rootfs%/}/${_target#/}"
+        fi
+    done
+
+    echo "$_target"
+}
+
+_add_resolv_conf() {
+    local _src=$(_resolve_link "/etc/resolv.conf")
+    local _dest=$(_resolve_link "$chroot_dir/etc/resolv.conf" "$chroot_dir")
+
+    # If we don't have a source resolv.conf file, there's nothing useful we can do.
+    if [[ ! -e $_src ]]; then
+        return
+    fi
+
+    if [[ ! -e $_dest ]]; then
+        # There are two reasons the destination might not exist:
+        #
+        #   1. There may be no resolv.conf in the chroot.  In this case, $_dest won't exist,
+        #      and it will be equal to $1/etc/resolv.conf.  In this case, we'll just exit.
+        #      The chroot environment must not be concerned with DNS resolution.
+        #
+        #   2. $1/etc/resolv.conf is (or resolves to) a broken link.  The environment
+        #      clearly intends to handle DNS resolution, but something's wrong.  Maybe it
+        #      normally creates the target at boot time.  We'll (try to) take care of it by
+        #      creating a dummy file at the target, so that we have something to bind to.
+
+        # Case 1.
+        if [[ "$_dest" = "$chroot_dir/etc/resolv.conf" ]]; then
+            return
+        fi
+
+        # Case 2.
+        install -D -m 644 /dev/null "$_dest"
+    fi
+
+    chroot_add_mount --bind "$_src" "$_dest"
+}
+
+# chroot_setup <chroot_dir>
+# $1: the directory to chroot into
+# when using `chroot_setup`, `chroot_teardown` must be call to clean up
+# for example, `trap chroot_teardown EXIT`
+chroot_setup() {
+    chroot_active_mounts=()
+    chroot_dir="$1"
+
+    if ! mountpoint -q "$chroot_dir"; then
+        info "\"$chroot_dir\" is not a mountpoint. This may have undesirable side effects."
+        info "Bind mounting \"$chroot_dir\" to itself."
+        chroot_add_mount --bind "$chroot_dir" "$chroot_dir"
+    fi
+
+    chroot_add_mount -t sysfs -o ro,nosuid,nodev,noexec sys "$chroot_dir/sys"
+    chroot_add_mount -t proc -o nosuid,nodev,noexec proc "$chroot_dir/proc"
+    chroot_add_mount -t devtmpfs -o nosuid,mode=755 udev "$chroot_dir/dev"
+    chroot_add_mount -t devpts -o nosuid,noexec,gid=5,mode=620,ptmxmode=000 devpts "$chroot_dir/dev/pts"
+    chroot_add_mount -t tmpfs -o nosuid,nodev,noexec,mode=755 run "$chroot_dir/run"
+    chroot_add_mount -t tmpfs -o nosuid,nodev shm "$chroot_dir/dev/shm"
+    chroot_add_mount -t tmpfs -o nosuid,nodev,mode=1777 tmp "$chroot_dir/tmp"
+    if [[ -d "$chroot_dir/sys/firmware/efi/efivars" ]]; then
+        chroot_add_mount -t efivarfs -o nosuid,nodev,noexec efivarfs "$chroot_dir/sys/firmware/efi/efivars"
+    fi
+
+    _add_resolv_conf
+}
+
+chroot_teardown() {
+    if (( ${#chroot_active_mounts[@]} )); then
+        umount -v "${chroot_active_mounts[@]}"
+    fi
+    chroot_active_mounts=()
+}
+
+chroot_run() {
+    # SHELL=/bin/bash LC_ALL=C PATH="$CHROOT_PATH" unshare --fork --pid chroot "$@"
+    SHELL=/bin/bash PATH="$CHROOT_PATH" unshare --fork --pid chroot "$@"
+}
+
+debug "${BASH_SOURCE[0]} sourced"
+
+### error codes
+# 1: This script must be run as root
---
+++ bash/lib/pacman.sh
@@ -0,0 +1,55 @@
+#
+# pacman.sh
+#
+
+################################################################################
+
+if [[ -n "${__PACMAN__:-}" ]]; then
+    return
+fi
+
+declare -i __PACMAN__=1
+
+################################################################################
+
+### constants & variables
+EXTENSION="tar.zst"
+#                 epoch:                         pkgver-pkgrel-                    arch.pkg.tar.zst
+PKGNAME_REGEX="\([0-9]+:\)?\([0-9a-zA-Z]+\(\.\|\+\)?\)+-[0-9]+-\(x86_64\|aarch64\|any\).pkg.$EXTENSION"
+
+### functions
+get_pkg_file() {
+    local _pkg="$1"
+    local _dir="$2"
+    #               pkgname-
+    local _pkg_regex="$_pkg-$PKGNAME_REGEX"
+    if [[ -d "$_dir" ]]; then
+        find "$_dir" -type f -regex "$_dir/$_pkg_regex" -printf "%T@ %f\n" | LC_ALL=C sort -n | tail -n 1 | cut -d ' ' -f 2
+    fi
+}
+
+# get_pkgbase <pkgname>
+# $1: pkgname
+get_pkgbase() {
+    local _pkgbase
+    case "$1" in
+        device-mapper) _pkgbase=lvm2 ;;
+        linux-headers) _pkgbase=linux ;;
+        *) _pkgbase="$1" ;;
+    esac
+    echo "$_pkgbase"
+}
+
+# get_pkgnames <pkgbase>
+# $1: pkgbase
+get_pkgnames() {
+    local -a _pkgnames
+    case "$1" in
+        linux) _pkgnames=("linux" "linux-headers") ;;
+        lvm2) _pkgnames=("lvm2" "device-mapper") ;;
+        *) _pkgnames=("$1") ;;
+    esac
+    echo "${_pkgnames[@]}"
+}
+
+debug "${BASH_SOURCE[0]} sourced"
