29ecabdd6b02a08f074d32aff1dbe8543c13900f
KZL <lz3450@outlook.com>
2025-06-25 01:45:14 -0500

    ubuntu/install: desktop

--- ubuntu/install/desktop-install.sh
+++ ubuntu/install/desktop-install.sh
@@ -10,71 +10,26 @@ if [[ $EUID -ne 0 ]]; then
     exit 255
 fi
 
+. /etc/os-release
+LOG_DIR="log/$UBUNTU_CODENAME"
+TO_INSTALL_PKGLIST_FILE="$LOG_DIR/desktop_to_install_pkgs.txt"
+INSTALLED_PKGLIST_FILE="$LOG_DIR/desktop_installed_pkgs.txt"
+MANUAL_INSTALLED_PKGLIST_FILE="$LOG_DIR/desktop_manual_installed_pkgs.txt"
+
 common_deb_pkgs=(
-  ### depends
-  fonts-dejavu-core
-  gdm3
-  gnome-shell
-  gnome-shell-extension-appindicator
-  gnome-shell-extension-desktop-icons-ng
-  gnome-shell-extension-ubuntu-dock
-  nautilus
-  ubuntu-session
-  ubuntu-settings
-  ### recommends
-  fonts-noto-cjk
-  fonts-noto-color-emoji
-  fonts-ubuntu
-  fwupd
-  gnome-calculator
-  gnome-characters
-  # gnome-disk-utility
-  gnome-font-viewer
-  gnome-logs
-  # gnome-power-manager
-  gnome-remote-desktop
-  gnome-system-monitor
-  gnome-terminal
-  gsettings-ubuntu-schemas
-  seahorse
-  ubuntu-wallpapers
-  yaru-theme-gnome-shell
-  yaru-theme-gtk
-  yaru-theme-icon
-  yaru-theme-sound
+  ubuntu-desktop-minimal
   ### custom
   landscape-client
   tmux
 )
 jammy_deb_pkgs=(
-  ### depends
-  fonts-freefont-ttf
-  ### recommends
-  fonts-opensymbol
-  gedit
 )
 noble_deb_pkgs=(
-  # depends
-  gnome-shell-extension-ubuntu-tiling-assistant
-  # recommends
-  # baobab
-  fonts-noto-core
-  # gnome-clocks
-  gnome-text-editor
 )
 questing_deb_pkgs=(
-  ### depends
-  gnome-shell-extension-ubuntu-tiling-assistant
-  ### recommends
-  # baobab
-  fonts-noto-core
-  # gnome-clocks
-  gnome-text-editor
-  # papers
 )
 declare -a deb_pkgs
 
-. /etc/os-release
 case "$UBUNTU_CODENAME" in
     jammy)      deb_pkgs=("${common_deb_pkgs[@]}" "${jammy_deb_pkgs[@]}") ;;
     noble)      deb_pkgs=("${common_deb_pkgs[@]}" "${noble_deb_pkgs[@]}") ;;
@@ -82,15 +37,20 @@ case "$UBUNTU_CODENAME" in
     *)          echo "Unknown suite \"$1\"" && exit 1
 esac
 
+# log files
+
+mkdir -vp -- "$LOG_DIR"
+touch "$TO_INSTALL_PKGLIST_FILE" "$INSTALLED_PKGLIST_FILE" "$MANUAL_INSTALLED_PKGLIST_FILE"
+chown -R ${SUDO_UID:-0}:${SUDO_GID:-0} "$LOG_DIR"
+
 # install packages
 apt-get update
 apt-get upgrade -y
-mkdir -vp -- "log/$UBUNTU_CODENAME"
-apt-get install --no-install-recommends -s "${deb_pkgs[@]}" | grep "^Inst" | awk '{print $2}' | sort -n > "log/$UBUNTU_CODENAME/desktop_to_install_pkgs.txt"
-apt-get install --no-install-recommends -y "${deb_pkgs[@]}"
+apt-get install --no-install-recommends -s "${deb_pkgs[@]}" | grep "^Inst" | awk '{print $2}' | sort -n > "$TO_INSTALL_PKGLIST_FILE"
+# apt-get install --no-install-recommends -y "${deb_pkgs[@]}"
 apt-get autoremove --purge -y
 
-dpkg --get-selections | awk '{print $1}' | sed -e 's/:amd64//g' > "log/$UBUNTU_CODENAME/desktop_installed_pkgs.txt"
-apt-mark showmanual > "log/$UBUNTU_CODENAME/desktop_manual_installed_pkgs.txt"
+dpkg --get-selections | awk '{print $1}' | sed -e 's/:amd64//g' > "$INSTALLED_PKGLIST_FILE"
+apt-mark showmanual > "$MANUAL_INSTALLED_PKGLIST_FILE"
 
 echo "Successfully installed desktop packages for Ubuntu $UBUNTU_CODENAME"
