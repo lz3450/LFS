1b4514eb4b15d3911993477b4a09a3ff4dee8d10
KZL <lz3450@outlook.com>
2025-06-26 00:08:55 -0500

    rpi/install: minor fix for rpi

--- ubuntu/install/02-install-app.sh
+++ ubuntu/install/02-install-app.sh
@@ -12,6 +12,7 @@ fi
 
 . /etc/os-release
 LOG_DIR="log/$UBUNTU_CODENAME"
+ARCH=$(dpkg --print-architecture)
 
 ### utils
 utils_deb_pkgs=(
@@ -27,15 +28,21 @@ apt-get install --no-install-recommends -y "${utils_deb_pkgs[@]}"
 dpkg --get-selections | awk '{print $1}' | sed -e 's/:amd64//g' -e 's/:arm64//g' > "$LOG_DIR/utils_installed_pkgs.txt"
 
 ### microsoft
+microsoft_deb_pkgs=(
+    code
+)
+
 cat > /etc/apt/sources.list.d/vscode.sources << EOF
 Types: deb
 URIs: https://packages.microsoft.com/repos/code/
 Suites: stable
 Components: main
-Architectures: amd64
+Architectures: $ARCH
 Signed-By: /usr/share/keyrings/microsoft.gpg
 EOF
-cat > /etc/apt/sources.list.d/microsoft-edge.sources << EOF
+if [[ "$ARCH" == "amd64" ]]; then
+    microsoft_deb_pkgs+=(microsoft-edge-stable)
+    cat > /etc/apt/sources.list.d/microsoft-edge.sources << EOF
 Types: deb
 URIs: https://packages.microsoft.com/repos/edge/
 Suites: stable
@@ -43,12 +50,13 @@ Components: main
 Architectures: amd64
 Signed-By: /usr/share/keyrings/microsoft.gpg
 EOF
+fi
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | install -o root -g root -m 644 /dev/stdin /usr/share/keyrings/microsoft.gpg
 
 apt-get update
-apt-get install --no-install-recommends -s microsoft-edge-stable code | grep "^Inst" | awk '{print $2}' | LC_ALL=C sort -n > "$LOG_DIR/microsoft_to_install_pkgs.txt"
-apt-get install --no-install-recommends -y microsoft-edge-stable code
+apt-get install --no-install-recommends -s "${microsoft_deb_pkgs[@]}" | grep "^Inst" | awk '{print $2}' | LC_ALL=C sort -n > "$LOG_DIR/microsoft_to_install_pkgs.txt"
+apt-get install --no-install-recommends -y "${microsoft_deb_pkgs[@]}"
 dpkg --get-selections | awk '{print $1}' | sed -e 's/:amd64//g' -e 's/:arm64//g' > "$LOG_DIR/microsoft_installed_pkgs.txt"
 rm -vf -- /etc/apt/sources.list.d/microsoft-edge.list
 
