fa998396c92056ce9f9d0dc93e62eb75dee34db0
KZL <lz3450@outlook.com>
2025-07-25 18:56:08 -0500

    mkrootfs: deps

--- mkrootfs/install-dependencies.sh
+++ mkrootfs/install-dependencies.sh
@@ -2,9 +2,12 @@
 
 sudo apt-get update
 sudo apt-get install -y \
+    binfmt-support \
     dosfstools \
     isomd5sum \
+    qemu-user-static \
     xorriso
 sudo pacman -Sy --noconfirm \
+    debootstrap \
     dracut \
     erofs-utils
