6bdabe8d1e86bfd611fb0ebf12ac9e195fe837eb
KZL <lz3450@outlook.com>
2025-08-03 10:43:42 -0500

    config: rsyslog

--- config/rsyslog/dsldevice-attlocal-net.sh
+++ config/rsyslog/dsldevice-attlocal-net.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-LOG_FILE=/var/log/dsldevice-attlocal-net.log
-
 set -e
 
+LOG_FILE=/var/log/dsldevice-attlocal-net.log
+
 sudo touch "$LOG_FILE"
-sudo chmod 660 "$LOG_FILE"
 sudo chown syslog:adm "$LOG_FILE"
+sudo chmod 660 "$LOG_FILE"
 
 sudo cp dsldevice-attlocal-net.conf /etc/rsyslog.d/
 sudo systemctl restart rsyslog
