d7dcb82221c88466c354f0700052ea9105eb98d4
KZL <lz3450@outlook.com>
2025-09-22 03:00:53 -0500

    scripts/rpi: ubuntu

--- scripts/rpi/ubuntu/02-install-app.sh
+++
@@ -1,1 +0,0 @@
-../../ubuntu/02-install-app.sh
--- scripts/rpi/ubuntu/11-config-system.sh
+++ scripts/rpi/ubuntu/11-config-system.sh
@@ -1,1 +1,36 @@
-../../ubuntu/11-config-system.sh
+#!/bin/bash
+#
+# 11-config-system.sh
+#
+
+set -e
+
+if (( EUID != 0 )); then
+    echo "This script must be run as root"
+    exit 255
+fi
+
+. /etc/os-release
+
+### /tmp
+sudo systemctl enable --now /usr/share/systemd/tmp.mount
+
+### network-manager
+if [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
+    tee /etc/netplan/00-default.yaml << EOF
+network:
+  version: 2
+  renderer: NetworkManager
+EOF
+    chmod 600 /etc/netplan/00-default.yaml
+    netplan apply
+    systemctl disable wpa_supplicant@wlan0.service
+    systemctl disable systemd-networkd.service
+    systemctl enable wpa_supplicant.service
+    systemctl enable NetworkManager.service
+else
+    echo "Unsupported Ubuntu suite: $UBUNTU_CODENAME"
+    exit 1
+fi
+
+echo "Successfully configured system settings for Ubuntu $UBUNTU_CODENAME"
