609f8f2f7b6c03886b68b03829e08dc0442cb702
KZL <lz3450@outlook.com>
2025-10-17 13:23:38 -0500

    python: pytorch

--- python/pytorch/build.sh
+++ python/pytorch/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # build.sh
 #
@@ -12,27 +12,31 @@ umask 0022
 
 ################################################################################
 
-PYTORCH_VERSION=v2.8.0
-
 export PATH="/opt/bin:/usr/local/cuda/bin:$PATH"
 
-if [[ ! -d "pytorch" ]]; then
-    git clone --depth 10 -b $PYTORCH_VERSION --recurse-submodules --shallow-submodules https://github.com/pytorch/pytorch
-    cd pytorch
-    git submodule sync
-else
-    cd pytorch
-    rm -rf build tmp
+PYTORCH_VERSION=v2.9.0
+
+if [[ ! -d "pytorch-$PYTORCH_VERSION" ]]; then
+    if [[ ! -f "pytorch-$PYTORCH_VERSION.tar.zst" ]]; then
+        git clone --depth 10 -b $PYTORCH_VERSION --recurse-submodules --shallow-submodules https://github.com/pytorch/pytorch pytorch-$PYTORCH_VERSION
+        tar --zstd -cf pytorch-$PYTORCH_VERSION.tar.zst pytorch-$PYTORCH_VERSION
+    else
+        tar -xf pytorch-$PYTORCH_VERSION.tar.zst
+    fi
 fi
 
+cd pytorch-$PYTORCH_VERSION
+
 sed -i \
-    -e '/cmake/d' \
     -e '/lintrunner/d' \
-    -e '/setuptools/d' \
-    requirements.txt
-python3 -m pip wheel --wheel-dir ~/wheels --no-binary :all: -r requirements.txt
-python3 -m pip install --user -U --no-index --find-links ~/wheels -r requirements.txt
+    pyproject.toml
+
+python3 -m pip wheel --wheel-dir ~/wheels --no-binary :all: --group dev
+python3 -m pip install --user -U --no-index --find-links ~/wheels --group dev
+python3 -m pip wheel --wheel-dir ~/wheels --no-binary :all: --only-binary mkl-static,intel-openmp,tbb,mkl-include mkl-static
+python3 -m pip install --user -U --no-index --find-links ~/wheels mkl-static
 
 # CMAKE_ONLY=1 python3 setup.py build > ../build.log 2>&1
 # ccmake build
-python3 -m pip -v wheel --no-build-isolation .
+python3 -m pip -v wheel --wheel-dir ~/wheels --no-binary :all: --no-build-isolation .
+tar --zstd -cf pytorch-$PYTORCH_VERSION-build.tar.zst pytorch-$PYTORCH_VERSION
