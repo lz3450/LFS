067f5a3d02501d3b2f502093663a6598253f33f3
KZL <lz3450@outlook.com>
2026-04-18 19:51:35 -0500

    scripts: ubuntu

--- scripts/ubuntu/10-config-rdp.sh
+++ scripts/ubuntu/10-config-rdp.sh
@@ -12,21 +12,21 @@ fi
 
 . /etc/os-release
 
-CERT_DIR="$HOME"/.config/gnome-remote-desktop/certificates
-KEY="$CERT_DIR"/grd-tls.key
-CSR="$CERT_DIR"/grd-tls.csr
-CRT="$CERT_DIR"/grd-tls.crt
+CERT_DIR="$HOME"/.local/share/gnome-remote-desktop/certificates
+KEY="$CERT_DIR"/rdp-tls.key
+CSR="$CERT_DIR"/rdp-tls.csr
+CRT="$CERT_DIR"/rdp-tls.crt
 
 sudo nano /etc/gdm3/custom.conf
-if [[ $UBUNTU_CODENAME == "noble" ]]; then
-    sudo systemctl disable gnome-remote-desktop.service
-fi
+# if [[ $UBUNTU_CODENAME == "noble" ]]; then
+#     sudo systemctl disable gnome-remote-desktop.service
+# fi
 systemctl --user enable --now gnome-remote-desktop.service
 
 if [[ ! -d  "$CERT_DIR" ]]; then
     mkdir -p "$CERT_DIR"
-    openssl genrsa -out "$CERT_DIR"/grd-tls.key 4096
-    openssl req -new -key "$KEY" -out "$CERT_DIR"/grd-tls.csr
+    openssl genrsa -out "$KEY" 4096
+    openssl req -new -key "$KEY" -out "$CSR"
     openssl x509 -req -days 365 -signkey "$KEY" -in "$CSR" -out "$CRT"
 fi
 
@@ -34,6 +34,7 @@ grdctl rdp set-tls-cert "$CRT"
 grdctl rdp set-tls-key "$KEY"
 grdctl rdp enable
 grdctl rdp disable-view-only
+grdctl rdp set-port 34589
 
 ./unlock_remote_desktop.sh
 read -r -p "Enter rdp username: " username
