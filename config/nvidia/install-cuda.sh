57f348f14d28d77689b75cce6975907cf3ee3ffd
KZL <lz3450@outlook.com>
2026-04-23 03:03:36 -0500

    config: nvidia

--- config/nvidia/install-cuda.sh
+++ config/nvidia/install-cuda.sh
@@ -14,11 +14,11 @@ umask 0022
 
 # https://developer.nvidia.com/cuda-toolkit-archive
 
-CUDA_VERSION=12.8.1
+CUDA_VERSION=13.0.3
 
 WORKDIR=/tmp/cuda
 DOWNLOADS_DIR="$HOME/Downloads"
-RUNFILE=cuda_${CUDA_VERSION}_570.124.06_linux.run
+RUNFILE=cuda_13.0.3_580.126.20_linux.run
 
 if [[ ! -f "$DOWNLOADS_DIR/$RUNFILE" ]]; then
     wget -P "$DOWNLOADS_DIR" https://developer.download.nvidia.com/compute/cuda/$CUDA_VERSION/local_installers/$RUNFILE
@@ -27,3 +27,5 @@ fi
 sudo rm -rf "$WORKDIR"
 mkdir -p "$WORKDIR"
 sudo bash "$DOWNLOADS_DIR/$RUNFILE" --tmpdir="$WORKDIR"
+
+cp -v /var/log/cuda-installer.log cuda-install.log
--- config/nvidia/install-cudnn.sh
+++ config/nvidia/install-cudnn.sh
@@ -15,12 +15,11 @@ umask 0022
 # https://docs.nvidia.com/deeplearning/cudnn/installation/latest/linux.html#tarball-installation
 # https://developer.download.nvidia.com/compute/cudnn/redist/
 
-CUDNN_VERSION=9.12.0.46
+TARBALL=cudnn-linux-x86_64-9.16.0.29_cuda13-archive.tar.xz
 
 WORKDIR=/tmp/cudnn
 DOWNLOADS_DIR="$HOME/Downloads"
-TARBALL=cudnn-linux-x86_64-${CUDNN_VERSION}_cuda12-archive.tar.xz
-CUDNNDIR=$WORKDIR/cudnn-linux-x86_64-${CUDNN_VERSION}_cuda12-archive
+CUDNNDIR=$WORKDIR/${TARBALL%.tar.xz}
 
 if [[ ! -f "$DOWNLOADS_DIR/$TARBALL" ]]; then
     wget -P "$DOWNLOADS_DIR" https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/$TARBALL
