385b67f50da3d9d92d073949975bffc1efdc228e
KZL <lz3450@outlook.com>
2026-06-15 01:56:06 -0500

    config: nvidia

--- config/nvidia/install-driver.sh
+++ config/nvidia/install-driver.sh
@@ -16,7 +16,7 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 ################################################################################
 
-DRIVER_VERSION=580.142
+DRIVER_VERSION=580.159.04
 
 WORKDIR=/tmp/nvidia
 DOWNLOADS_DIR="$HOME/Downloads"
